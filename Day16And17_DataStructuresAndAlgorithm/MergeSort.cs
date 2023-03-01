c71211a5599d74ba6442be85bb7a70504da61bd6
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-03-01 20:47:47 +0530

    Display prime number in range UC7_PrimeNumberCheck

---
+++ Day16And17_DataStructuresAndAlgorithm/MergeSort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16And17_DataStructuresAndAlgorithm
+{
+    public class MergeSort
+    {
+        public static void Merge_Sort_Opearation()
+        {
+
+        }
+    }
+}
---
+++ Day16And17_DataStructuresAndAlgorithm/PrimeNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16And17_DataStructuresAndAlgorithm
+{
+    public class PrimeNumber
+    {
+        public static void Prime_Number_Range()
+        {
+            int Start, Range, count = 0;
+            Console.WriteLine("Enter the starting range");
+            Start = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the range");
+            Range = Convert.ToInt32(Console.ReadLine());
+            for (int val1 = 1; val1 <= Range; val1++)
+            {
+                for (int val2 = 1; val2<= Range; val2++)
+                {
+                    if (val1%val2 == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    Console.WriteLine(val1);
+                }
+                count = 0;
+            }
+        }
+    }
+}
