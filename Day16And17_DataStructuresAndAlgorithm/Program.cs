b5690c17e678981482f85522a38a0adddfd6cc3a
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-03-01 19:24:27 +0530

    Adding welcome message

---
+++ Day16And17_DataStructuresAndAlgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day16And17_DataStructuresAndAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the DataStructure and algorithms Practices");
+        }
+    }
+}
