2999f1c56a706ce74cf287762ef9e6fcb330359a
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-03-01 21:59:31 +0530

    Merge sort technique UC5_MergeSort

--- Day16And17_DataStructuresAndAlgorithm/MergeSort.cs
+++ Day16And17_DataStructuresAndAlgorithm/MergeSort.cs
@@ -8,9 +8,58 @@ namespace Day16And17_DataStructuresAndAlgorithm
 {
     public class MergeSort
     {
-        public static void Merge_Sort_Opearation()
+        public static void Merge_Sort_Opearation(int[] arr, int val1, int val2,int val3)
         {
-
+            int num1 , num2 , num3 ;
+            int n1= val2-val1+1;
+            int n2 = val3 - val2;
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            for(num1=0; num1<n1;num1++)
+            {
+                L[num1] = arr[val1 + num1];   
+            }
+            for(num2=0;num2<n2;num2++)
+            {
+                R[num2] = arr[val2 + 1 + num2];
+            }
+            num1 = 0; num2 = 0; num3=0;
+            while(num1 <n1 && num2 < n2)
+            {
+                if (L[num1] <= R[num2])
+                {
+                    arr[num3] = L[num1];
+                    num1++;
+                }
+                else
+                {
+                    arr[num3] = R[num2];
+                    num2++;
+                }
+                num3++;
+            }
+            while(num1 < n1)
+            {
+                arr[num3] = L[num1];
+                num1++;
+                num3++;
+            }
+            while(num2 < n2)
+            {
+                arr[num3] = R[num2];
+                num2++;
+                num3++;
+            }
+        }
+         public static void mergesort(int[] arr , int val1,int val3)
+        {
+            if(val1 < val3)
+            {
+                int val2 = (val1 + val3) / 2;
+                mergesort(arr , val2, val3);
+                mergesort(arr , val2 + 1, val3);
+                Merge_Sort_Opearation(arr ,val1, val2, val3);
+            }
         }
     }
 }
--- Day16And17_DataStructuresAndAlgorithm/Program.cs
+++ Day16And17_DataStructuresAndAlgorithm/Program.cs
@@ -7,7 +7,7 @@ namespace Day16And17_DataStructuresAndAlgorithm
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the DataStructure and algorithms Practices");
-            Console.WriteLine("1.Permutation Of String\n4.Bubble Sort\n6.Anagram\n7.Prime Number");
+            Console.WriteLine("1.Permutation Of String\n4.Bubble Sort\n5.Merge Sort\n6.Anagram\n7.Prime Number");
             Console.WriteLine("Choose option from above");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
@@ -20,12 +20,29 @@ namespace Day16And17_DataStructuresAndAlgorithm
                 case 4:
                     BubbleSort.Bubble_Sort_Operation();
                     break;
+                case 5:
+                    int[] arr = new int[6] { 76, 89, 23, 1, 55, 78 };
+                    int n = 6, num;
+                    Console.WriteLine("Merge sort ");
+                    Console.WriteLine("Initial array is:");
+                    for(num= 0; num < arr.Length;num++)
+                    {
+                        Console.WriteLine(arr[num]+" ");
+                    }
+                    MergeSort.mergesort(arr, 0, n - 1);
+                    Console.WriteLine("Sorted array is:");
+                    for(num= 0; num < n;num++)
+                    {
+                        Console.WriteLine(arr[num]+" ");
+                    }
+                    break;
                 case 6:
                     Anagram.Anagram_Operation();
                     break;
                 case 7:
                     PrimeNumber.Prime_Number_Range();
                     break;
+
             }
         }
     }
