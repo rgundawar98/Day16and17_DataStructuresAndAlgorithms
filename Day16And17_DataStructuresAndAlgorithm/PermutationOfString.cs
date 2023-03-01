2247ea5f4cc8f1459f69ed15c1ff1c8db323a67f
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-03-01 19:48:17 +0530

    Calculating Permutation of string UC1_PermutationOfString

---
+++ Day16And17_DataStructuresAndAlgorithm/PermutationOfString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16And17_DataStructuresAndAlgorithm
+{
+    public class PermutationOfString
+    {
+        public static void Permute(string str ,int startindex ,int endindex)
+        {
+            if(startindex == endindex)
+                Console.WriteLine(str);
+            else
+            {
+                for(int i=1;i<=endindex;i++)
+                {
+                    str = Swap(str, startindex, endindex);
+                    Permute(str, startindex+1, endindex);
+                    str = Swap(str, startindex+1, endindex);
+                }
+            }
+        }
+        public static string Swap(string val , int val1 , int val2)
+        {
+            char temp;
+            char[] charArray = val.ToCharArray();
+            temp = charArray[val1];
+            charArray[val1] = charArray[val2];
+            charArray[val2] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
--- Day16And17_DataStructuresAndAlgorithm/Program.cs
+++ Day16And17_DataStructuresAndAlgorithm/Program.cs
@@ -7,6 +7,17 @@ namespace Day16And17_DataStructuresAndAlgorithm
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the DataStructure and algorithms Practices");
+            Console.WriteLine("1.Permutation Of String");
+            Console.WriteLine("Choose option from above");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    string str = "ABC";
+                    int len =str.Length;
+                    PermutationOfString.Permute(str ,0,len-1);
+                    break;
+            }
         }
     }
 }
